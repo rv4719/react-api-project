3f21e19020e3fcdcdca7fab2e123ad4ea10ee85d
Rosa Vasconcelos <rcandidacursos@yahoo.com>
2024-03-22 22:34:17 -0300

    inserção da opção de busca

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 /*
+Principais Funcionalidades:
+- Exibição de Lista de Fotos:
+O componente PhotoList exibe uma lista de fotos em cartões clicáveis.
+O usuário pode selecionar o número de fotos exibidas por página e a ordenação das fotos.
+Paginação e Ordenação:
 
+O usuário pode selecionar o número de itens por página entre várias opções.
+O usuário pode ordenar as fotos por título, ID ou ID do álbum.
+
+- Visualização de Detalhes da Foto:
+Ao clicar em uma foto, o usuário pode ver seus detalhes, como título, ID e ID do álbum, no componente PhotoDetails.
+Decisões de Design:
+
+Usabilidade:
+A interface fornece controles claros para selecionar o número de itens por página e a ordenação das fotos, melhorando a usabilidade do aplicativo.
+
+Estilo Visual:
+O Material-UI é utilizado para criar uma interface visualmente atraente e consistente.
+Interatividade:
+Os cartões de foto na lista são tornados clicáveis para permitir uma interação fácil e intuitiva.
+Escalabilidade:
+A aplicação é construída de forma a lidar com grandes conjuntos de dados, permitindo a paginação e a ordenação eficientes das fotos.
 */
 
 import React, { useState } from 'react';
--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,14 +1,6 @@
-/* - Este componente é responsável por exibir a lista de fotos.
-   - Utiliza o Material-UI para renderizar componentes como `Grid`, `Card`, `Select`, `MenuItem`, etc.
-   - Utiliza estados para controlar o número de itens por página, a página atual e o critério de ordenação.
-   - Usa o hook `useEffect` para buscar fotos da API quando os estados relevantes mudam.
-   - Fornece controles para selecionar o número de itens por página e o critério de ordenação.
-   - Cada foto é representada por um cartão clicável que permite visualizar os detalhes da foto.
-*/
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Grid, Card, CardMedia, CardContent, Typography, Select, MenuItem } from '@mui/material';
+import { Grid, Card, CardMedia, CardContent, Typography, Select, MenuItem, TextField } from '@mui/material';
 import './styles.css';
 
 const PhotoList = ({ onPhotoClick }) => {
@@ -16,16 +8,17 @@ const PhotoList = ({ onPhotoClick }) => {
   const [pageSize, setPageSize] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
   const [sortBy, setSortBy] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=${pageSize}&_sort=${sortBy}`)
+    axios.get(`https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=${pageSize}&_sort=${sortBy}&q=${searchTerm}`)
       .then(response => {
         setPhotos(response.data);
       })
       .catch(error => {
         console.error('Error fetching photos:', error);
       });
-  }, [currentPage, pageSize, sortBy]);
+  }, [currentPage, pageSize, sortBy, searchTerm]);
 
   const handlePageSizeChange = (event) => {
     setPageSize(event.target.value);
@@ -40,20 +33,27 @@ const PhotoList = ({ onPhotoClick }) => {
     setSortBy(event.target.value);
   };
 
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div className="photo-list-container">
-      <Typography variant="h5" gutterBottom>Clique na imagem desejada abaixo para ver os detalhes:</Typography>
+      <Typography variant="h5" gutterBottom>Clique na imagem desejada para exibir os detalhes:</Typography>
+      <TextField
+        label="Buscar por nome"
+        value={searchTerm}
+        onChange={handleSearchTermChange}
+        style={{ marginBottom: '10px' }}
+      />
       <div className="controls">
-        <div className="pagination">
+        <div className="search">
           <Typography variant="body1">Itens por página:</Typography>
           <Select value={pageSize} onChange={handlePageSizeChange}>
             <MenuItem value={10}>10</MenuItem>
             <MenuItem value={20}>20</MenuItem>
             <MenuItem value={50}>50</MenuItem>
             <MenuItem value={100}>100</MenuItem>
-            <MenuItem value={500}>500</MenuItem>
-            <MenuItem value={1000}>1000</MenuItem>
-            <MenuItem value={-1}>Todas</MenuItem>
           </Select>
         </div>
         <div className="sort">
@@ -80,7 +80,7 @@ const PhotoList = ({ onPhotoClick }) => {
                 alt={photo.title}
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
+                <Typography gutterBottom variant="h7" component="div">
                   {photo.title}
                 </Typography>
               </CardContent>
